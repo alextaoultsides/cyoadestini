13025e382aaa4d105364295daca7432dbc30b7c4
alextaoultsides <alextaoultsides@gmail.com>
2018-09-10 18:12:08 -0400

    Feat: Create Story and Answer objects

---
+++ Destini/Answer.swift
@@ -0,0 +1,20 @@
+//
+//  Answer.swift
+//  Destini
+//
+//  Created by atao1 on 9/10/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Answer {
+    
+    let answerText: String
+    let nextStory: Story
+    
+    init(answer: String) {
+        answerText = answer
+    }
+    
+}
--- Destini/DestiniViewController.swift
+++ Destini/DestiniViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DestiniViewController: UIViewController {
 
     // Our strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
